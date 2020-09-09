6027e7dc3d377d3012eed87b2da25d47c19282c2
ramil <ramilexe@gmail.com>
2020-09-09 23:06:43 +0300

    remove unused commented lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 # Runs the db migrations and starts the watcher services
 
-# Exit if the variable tests fail
-set -e
-set +x
-
-# Check the database variables are set
-# XXX set defaults, don't silently fail
-#test $DATABASE_HOSTNAME
-#test $DATABASE_NAME
-#test $DATABASE_PORT
-#test $DATABASE_USER
-#test $DATABASE_PASSWORD
-#test $IPFS_INIT
-#VDB_COMMAND=${VDB_COMMAND:-watch}
-set +e
-
 # Construct the connection string for postgres
 VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
 
