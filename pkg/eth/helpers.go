d3f30b621bcee0a4d5bcfdb791da52c563239584
Ramil Amerzyanov <ramilexe@gmail.com>
2021-03-10 19:18:32 +0300

    support custom chain config (#36)
    * support custom chain config
    
    * move loading config logic to a separate helper function

--- pkg/eth/helpers.go
+++ pkg/eth/helpers.go
@@ -17,7 +17,11 @@
 package eth
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/cmd/utils"
+	log "github.com/sirupsen/logrus"
+	"os"
 
 	sdtypes "github.com/ethereum/go-ethereum/statediff/types"
 
@@ -37,6 +41,28 @@ func ResolveToNodeType(nodeType int) sdtypes.NodeType {
 	default:
 		return sdtypes.Unknown
 	}
+}
+
+// LoadConfig loads chain config from json file
+func LoadConfig(chainConfigPath string) (*params.ChainConfig, error) {
+	file, err := os.Open(chainConfigPath)
+	if err != nil {
+		utils.Fatalf("Failed to read chain config file: %v", err)
+
+		return nil, err
+	}
+	defer file.Close()
+
+	chainConfig := new(params.ChainConfig)
+	if err := json.NewDecoder(file).Decode(chainConfig); err != nil {
+		utils.Fatalf("invalid chain config file: %v", err)
+
+		return nil, err
+	}
+
+	log.Infof("Using chain config from %s file. Content %+v", chainConfigPath, chainConfig)
+
+	return chainConfig, nil
 }
 
 // ChainConfig returns the appropriate ethereum chain config for the provided chain id
--- pkg/serve/config.go
+++ pkg/serve/config.go
@@ -48,6 +48,7 @@ const (
 
 	ETH_DEFAULT_SENDER_ADDR = "ETH_DEFAULT_SENDER_ADDR"
 	ETH_RPC_GAS_CAP         = "ETH_RPC_GAS_CAP"
+	ETH_CHAIN_CONFIG        = "ETH_CHAIN_CONFIG"
 	ETH_SUPPORTS_STATEDIFF  = "ETH_SUPPORTS_STATEDIFF"
 )
 
@@ -76,6 +77,7 @@ func NewConfig() (*Config, error) {
 	viper.BindEnv("ethereum.httpPath", shared.ETH_HTTP_PATH)
 	viper.BindEnv("ethereum.defaultSender", ETH_DEFAULT_SENDER_ADDR)
 	viper.BindEnv("ethereum.rpcGasCap", ETH_RPC_GAS_CAP)
+	viper.BindEnv("ethereum.chainConfig", ETH_CHAIN_CONFIG)
 	viper.BindEnv("ethereum.supportsStateDiff", ETH_SUPPORTS_STATEDIFF)
 
 	c.DBConfig.Init()
@@ -123,7 +125,12 @@ func NewConfig() (*Config, error) {
 			c.RPCGasCap = rpcGasCap
 		}
 	}
-	c.ChainConfig, err = eth.ChainConfig(nodeInfo.ChainID)
+	chainConfigPath := viper.GetString("ethereum.chainConfig")
+	if chainConfigPath != "" {
+		c.ChainConfig, err = eth.LoadConfig(chainConfigPath)
+	} else {
+		c.ChainConfig, err = eth.ChainConfig(nodeInfo.ChainID)
+	}
 	return c, err
 }
 
