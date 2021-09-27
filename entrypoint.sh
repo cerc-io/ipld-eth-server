b0674df3ce9cef2dff2f0294c5998108b15262ac
Arijit Das <arijitad.in@gmail.com>
2021-09-27 17:53:42 +0530

    Remove parallel migration in integration test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,20 @@
 # Construct the connection string for postgres
 VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
 
-# Run the DB migrations
-echo "Connecting with: $VDB_PG_CONNECT"
-echo "Running database migrations"
-./goose -dir migrations/vulcanizedb postgres "$VDB_PG_CONNECT" up
-rv=$?
 
-if [ $rv != 0 ]; then
-  echo "Could not run migrations. Are the database details correct?"
-  exit 1
+if [ "$RUN_DB_MIGRATION" != "no" ]
+then
+    # Run the DB migrations
+    echo "Connecting with: $VDB_PG_CONNECT"
+    echo "Running database migrations"
+    ./goose -dir migrations/vulcanizedb postgres "$VDB_PG_CONNECT" up
+    rv=$?
+
+    if [ $rv != 0 ]; then
+      echo "Could not run migrations. Are the database details correct?"
+      exit 1
+    fi
 fi
-
 
 echo "Beginning the ipld-eth-server process"
 
