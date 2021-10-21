6d676a342d1f0abe1b83ef100714046fcbc52547
Arijit Das <arijitad.in@gmail.com>
2021-10-21 14:54:42 +0530

    Upgrade geth-statediff to version v1.10.11-statediff-0.0.27

--- test/integration_suite_test.go
+++ test/integration_suite_test.go
@@ -1,12 +1,12 @@
 package integration_test
 
 import (
-	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
 )
 
 func TestIntegration(t *testing.T) {
