f733250afee34f1c860e0ab8e1d80b4d66a51349
i-norden <iansnorden@gmail.com>
2021-12-30 02:01:46 -0600

    update version meta

--- version/version.go
+++ version/version.go
@@ -19,10 +19,10 @@ package version
 import "fmt"
 
 const (
-	Major = 0       // Major version component of the current release
-	Minor = 3       // Minor version component of the current release
-	Patch = 9       // Patch version component of the current release
-	Meta  = "alpha" // Version metadata to append to the version string
+	Major = 0  // Major version component of the current release
+	Minor = 3  // Minor version component of the current release
+	Patch = 9  // Patch version component of the current release
+	Meta  = "" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
