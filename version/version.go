dadde8535c414daa611cdf05eaf7b7ea2435a14d
prathamesh0 <prathamesh.musale0@gmail.com>
2022-09-14 14:04:49 +0530

    Increase EVM timeout for eth-calls

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ import "fmt"
 const (
 	Major = 4       // Major version component of the current release
 	Minor = 1       // Minor version component of the current release
-	Patch = 5       // Patch version component of the current release
+	Patch = 9       // Patch version component of the current release
 	Meta  = "alpha" // Version metadata to append to the version string
 )
 
