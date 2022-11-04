5f6f277e41babcafb03fefead010d607cacd96f0
prathamesh0 <prathamesh.musale0@gmail.com>
2022-11-04 09:34:37 +0530

    Increase wait time for db setup in unit test job

--- .github/workflows/run_unit_test.sh
+++ .github/workflows/run_unit_test.sh
@@ -17,7 +17,7 @@ rm -f /tmp/git_head_ref /tmp/git_repository
 echo 'docker-compose up -d migrations ipld-eth-db'
 docker-compose up -d migrations ipld-eth-db
 trap "docker-compose down -v --remove-orphans; cd $start_dir ; rm -r $temp_dir" SIGINT SIGTERM ERR
-sleep 30
+sleep 60
 
 # Remove old logs so there's no confusion, then run test
 rm -f /tmp/test.log /tmp/return_test.txt
