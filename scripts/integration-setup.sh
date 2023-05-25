6eded51cf1295b4d14f71972ed20e329d406b349
Roy Crihfield <roy@manteia.ltd>
2023-05-25 21:48:12 +0800

    un-dev

--- scripts/integration-setup.sh
+++ scripts/integration-setup.sh
@@ -16,31 +16,20 @@ laconic_so="${LACONIC_SO:-laconic-so} --verbose --stack fixturenet-eth-loaded"
 
 set -x
 
-# # Build and deploy a cluster with only what we need from the stack
-# $laconic_so setup-repositories \
-#   --exclude cerc-io/ipld-eth-server,cerc-io/tx-spammer \
-#   --branches-file ./test/stack-refs.yml
-
-# $laconic_so build-containers \
-#   --exclude cerc/ipld-eth-server,cerc/keycloak,cerc/tx-spammer
-
-IMAGE_IPLD_ETH_DB=git.vdb.to/cerc-io/ipld-eth-db/ipld-eth-db:v5.0.2-alpha
-IMAGE_GETH=git.vdb.to/cerc-io/go-ethereum/go-ethereum:v1.11.5-statediff-5.0.5-alpha
-
-docker pull $IMAGE_IPLD_ETH_DB
-docker pull $IMAGE_GETH
-docker tag $IMAGE_IPLD_ETH_DB cerc/ipld-eth-db:local
-docker tag $IMAGE_GETH cerc/go-ethereum:local
+# Build and deploy a cluster with only what we need from the stack
+$laconic_so setup-repositories \
+  --exclude cerc-io/ipld-eth-server,cerc-io/tx-spammer \
+  --branches-file ./test/stack-refs.yml
 
 $laconic_so build-containers \
-  --exclude cerc/ipld-eth-server,cerc/keycloak,cerc/tx-spammer,cerc/go-ethereum,cerc/ipld-eth-db
+  --exclude cerc/ipld-eth-server,cerc/keycloak,cerc/tx-spammer
 
 $laconic_so deploy \
   --include fixturenet-eth,ipld-eth-db \
   --env-file $CONFIG_DIR/stack.env \
   --cluster test up
 
-# set +x
+set +x
 
 # Get IPv4 endpoint of geth file server
 bootnode_endpoint=$(docker port test-fixturenet-eth-bootnode-geth-1 9898 | head -1)
